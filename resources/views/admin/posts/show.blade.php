cd5964c296ab6a1728a5b6b7f9732c6e2dc12c1d
E-Lop <edmondolopez@gmail.com>
2022-09-06 16:21:39 +0200

    date formatted using Carbon

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <h1>{{ $post->title }}</h1>
 
-    <div><strong>Creato il: </strong>{{ $post->created_at }}</div>
-    <div><strong>Aggiornato il: </strong>{{ $post->updated_at }}</div>
+    <div><strong>Creato il: </strong>{{ $post->created_at->format('l d F Y') }}</div>
+    <div><strong>Aggiornato il: </strong>{{ $post->updated_at->format('l d F Y') }}</div>
     <div><strong>Slug: </strong>{{ $post->slug }}</div>
     <h3 class="mt-2"><strong>Contenuto del post:</strong></h3>
     <p>{{ $post->content }}</p>
