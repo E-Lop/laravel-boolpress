d89d9e570303be0d7d238562962bbf980da0edf0
E-Lop <edmondolopez@gmail.com>
2022-09-16 15:54:03 +0200

    added validation rules to form

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Lead;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request) {
         $data = $request->all();
+
+        // validazione dei dati
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'message' => 'required|max:60000',
+        ]);
+
+        // se la validazione fallisce
+        if($validator->fails()) {
+            // json restituisce success => false
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
 
         // salvataggio dati in db
         $new_lead = new Lead();
